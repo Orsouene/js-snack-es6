2fb2224b50ccba890a845f0a947fee2b24151736
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-12 19:57:42 +0100

    Corretto il bonus

--- script/script3.js
+++ script/script3.js
@@ -1,26 +1,33 @@
 // Bonus
-let myArray = [];
-let a = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-console.log(a);
-let b = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-console.log(b);
-function myFunction(myArray) {
-  let resultato;
-  let i = 1;
-
-  for (i = 1; i < b; i++) {
-    if (b > a) {
-      //   il resultato è b - 1 inizialmente e poi incremento la i di 1
-      resultato = b - i;
-      //   finche il resultato e magiore di a
-      if (resultato > a) {
-        //   console.log("resultato " + resultato);
-        myArray.push(resultato);
-        //   console.log(myArray);
-      }
-    }
+// un array come esempio
+let myArray = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
+// indici a
+let a = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+// stampare il valore di a
+console.log("Il valore di a è : " + a);
+// indici b
+let b = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
+// stampare il valore di b
+console.log("Il valore di b è : " + b);
+function myFunction(Array, a, b) {
+  // se a>0=b
+  if (a >= b) {
+    // Stampare il messaggio d'errore
+    console.log("a devi essere piu grande di b ");
+    return;
   }
-  return myArray;
+  //   se b-a =
+  else if (b - a === 1) {
+    // Stampare il messaggio d'errore
+    console.log(" il resultato di ( b - a ) devi essere >1");
+    return;
+  }
+  //   se b>a
+  else if (b > a) {
+    // Restituisco un nuovo array con gli elementi dell'altro array che si trovano negli indici da a+1 e( b (escluso da default ))
+    return Array.slice(a + 1, b);
+  }
 }
-const resultatoFinale = myFunction(myArray);
+// Stampare  i valori che hanno la posizione compresa tra a e b  :
+const resultatoFinale = myFunction(myArray, a, b);
 console.log(resultatoFinale);
